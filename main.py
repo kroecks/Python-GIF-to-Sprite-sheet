46529625f5c9d86178b0f940231d2690a40f78ba
Kenneth Roecks <kroecks@gmail.com>
2023-06-08 13:33:02 -0700

    Row calculation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from os import listdir
 from PIL import Image
+import math
 
 OUTPUT_SIZE = (64,64) # The output size of each frame (or tile or Sprite) of the animation
 MONOCHROME = False # Do you want the output file to be b/w?
@@ -10,17 +11,29 @@ for file in listdir():
         print(f"Size: {gif.size}")
         print(f"Frames: {gif.n_frames}")
 
+        totalCount = gif.n_frames
+        sqr = math.sqrt(totalCount)
+
+        gridSize = round(sqr + 0.5)
+
         if MONOCHROME:
             output = Image.new("1", (OUTPUT_SIZE[0] * gif.n_frames, OUTPUT_SIZE[1]), 0)
         else:
-            output = Image.new("RGB", (OUTPUT_SIZE[0] * gif.n_frames, OUTPUT_SIZE[1]))
+            output = Image.new("RGB", (gridSize * OUTPUT_SIZE[0], gridSize * OUTPUT_SIZE[1]))
 
         output_filename = f"icon_{gif.n_frames}_frames.bmp"
 
         for frame in range(0,gif.n_frames):
             gif.seek(frame)
             extracted_frame = gif.resize(OUTPUT_SIZE)
-            position = (OUTPUT_SIZE[0]*frame, 0)
+
+            div = frame / gridSize
+
+            row = int(div)
+            column = frame % gridSize
+
+            position = (int(OUTPUT_SIZE[0]*column), int(OUTPUT_SIZE[0]*row))
+
             output.paste(extracted_frame, position)
 
         if not MONOCHROME:
